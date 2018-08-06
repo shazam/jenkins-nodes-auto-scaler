6ac05a86a082988a6e9fad6c649e034accb2b398
Luca Naldini <luca.naldini@shazam.com>
2018-08-06 08:23:24 +0100

    Add timeout to compute engine api commands, to avoid getting stuck on status change

--- main.go
+++ main.go
@@ -592,28 +592,47 @@ func disableAllBuildBoxes() {
 	wg.Wait()
 }
 
-func waitForStatus(buildBox string, status string) error {
-	previousStatus := ""
-	for {
-		i, err := service.Instances.Get(*gceProjectName, *gceZone, buildBox).Do()
-		if nil != err {
-			log.Printf("Failed to get instance data for %s: %v\n", buildBox, err)
-			continue
+func waitForStatus(buildBox string, status string) {
+	completed := make(chan bool, 1)
+	quit := make(chan bool)
+
+	go func() {
+		previousStatus := ""
+		for {
+			select {
+			case <- quit:
+				return
+			default:
+			}
+
+			i, err := service.Instances.Get(*gceProjectName, *gceZone, buildBox).Do()
+			if nil != err {
+				log.Printf("Failed to get instance data for %s: %v\n", buildBox, err)
+				continue
+			}
+
+			if previousStatus != i.Status {
+				log.Printf("    %s -> %s\n", buildBox, i.Status)
+				previousStatus = i.Status
+			}
+
+			if i.Status == status {
+				log.Printf("    %s reached %s status\n", buildBox, status)
+				break
+			}
+
+			time.Sleep(time.Second * 3)
 		}
 
-		if previousStatus != i.Status {
-			log.Printf("  %s -> %s\n", buildBox, i.Status)
-			previousStatus = i.Status
-		}
+		completed <- true
+	}()
 
-		if i.Status == status {
-			log.Printf("==> %s is %s\n", buildBox, status)
-			return nil
-		}
-
-		time.Sleep(time.Second * 3)
+	select {
+	case <-completed:
+	case <-time.After(1 * time.Minute):
+		quit <- true
+		log.Printf("    %s did not reach %s status within a reasonable time\n", buildBox, status)
 	}
-	return nil
 }
 
 func getServiceWithCredsFile() (*compute.Service, error) {
